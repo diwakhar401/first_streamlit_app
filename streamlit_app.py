42becd90eea253e27a894cdbe8bcd3584bcdfcdf
diwakhar401 <108159012+diwakhar401@users.noreply.github.com>
2022-06-25 06:12:29 +0530

    Update streamlit_app.py
    fixed typo

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,5 @@
 import streamlit
 streamlit.title('My Parents New Healthy Dinner')
+streamlit.header('Breakfast Menu')
+streamlit.text('100g of Soaked Peanut and 2 Bananas')
+streamlit.text('Sprouts of Greengram, Readgram with Carrot Slices')

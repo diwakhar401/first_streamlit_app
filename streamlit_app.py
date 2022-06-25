1ee8ada6df620942b7f8b871a79e685a820156b0
diwakhar401 <108159012+diwakhar401@users.noreply.github.com>
2022-06-26 05:19:20 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,5 @@
 import streamlit
 streamlit.title('My Parents New Healthy Dinner')
 streamlit.header('Breakfast Menu')
-streamlit.text('100g of Soaked Peanut and 2 Bananas')
+streamlit.text('100g of Soaked Peanut and 2 Bananas U+1F34C')
 streamlit.text('Sprouts of Greengram, Readgram with Carrot Slices')

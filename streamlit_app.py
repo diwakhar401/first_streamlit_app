df2de367619d00623349be01765fce9e4f0d2cb8
diwakhar401 <108159012+diwakhar401@users.noreply.github.com>
2022-06-26 05:38:06 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,4 +4,4 @@ streamlit.header('Breakfast Menu')
 streamlit.text('100g of Soaked Peanut \N{Peanuts} and 2 Bananas \N{banana}\N{banana}')
 streamlit.text('Sprouts of Greengram, Readgram with Carrot Slices \N{carrot}')
 
-stramlit.header('\N{mango}\N{red apple} Make Your Own Fruit Diet \N{grapes}\N{kiwi fruit}')
+stramlit.header('\N{mango} \N{apple} Make Your Own Fruit Diet \N{grapes}\N{cherries}')

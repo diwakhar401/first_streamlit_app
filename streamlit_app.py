95d6f07eefb102f4e63a1987f26aedb809aa88f4
diwakhar401 <108159012+diwakhar401@users.noreply.github.com>
2022-06-26 05:57:54 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,6 +6,12 @@ streamlit.text('Sprouts of Greengram, Readgram with Carrot Slices \N{carrot}')
 
 streamlit.header('\N{mango}\N{banana}\N{Cucumber} Make Your Own Fruit Diet \N{grapes}\N{cherries}')
 
+
 import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+
+# Let's put a pick list here so they can pick the fruit they want to include 
+streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
+
+# Display the table on the page
 streamlit.dataframe(my_fruit_list)

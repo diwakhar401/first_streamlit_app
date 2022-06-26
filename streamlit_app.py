5ac61df6ce3087c74d5ed23d1654cb6c2249c6cd
diwakhar401 <108159012+diwakhar401@users.noreply.github.com>
2022-06-26 05:44:32 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,3 +5,5 @@ streamlit.text('100g of Soaked Peanut \N{Peanuts} and 2 Bananas \N{banana}\N{ban
 streamlit.text('Sprouts of Greengram, Readgram with Carrot Slices \N{carrot}')
 
 streamlit.header('\N{mango}\N{banana}\N{Cucumber} Make Your Own Fruit Diet \N{grapes}\N{cherries}')
+
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
